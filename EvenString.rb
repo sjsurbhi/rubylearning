10b41cf509fd130254c0390e0194731d865ad2cc
surbhi joshi <35285621+sjsurbhi@users.noreply.github.com>
2018-03-23 18:44:30 +0530

    Update EvenString.rb

--- EvenString.rb
+++ EvenString.rb
@@ -2,12 +2,14 @@ class EvenString
 	def EvenChar
 		string = 'Hello World'
  array = string.scan(/\w/)
+		#array = string.split(' ').join.split('')
+	b = []
  array.each_with_index do |sval,s|
  	if s%2 == 1
- 		b = array[s]
- 		puts b
- 	end
+ 		b << array[s]
+	end
  end
+	puts "Output of array is #{b}"
 end
 string = EvenString.new
 string.EvenChar
